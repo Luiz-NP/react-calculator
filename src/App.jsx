1dea17217ed958ad291d56746b893cb52a770e92
Luiz <gustavoooliveira111@gmail.com>
2022-12-11 21:03:30 -0300

    rendering components

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { Wrapper } from "./components/Wrapper";
+
+export function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-orange-500">
+      <Wrapper />
+    </div>
+  );
+};
